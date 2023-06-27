84e365cdd944116a2d72fa73c3d4bccc45945751
D-Freitas <davi.israel01@gmail.com>
2023-06-27 09:52:39 -0300

    test(aes256gcm): ensure this returns decrypted data

--- gcm_test.go
+++ gcm_test.go
@@ -13,4 +13,13 @@ func TestGCM(t *testing.T) {
 		encrypted, _ := sut.Encrypt(make([]byte, 12), []byte("any_data"), []byte("AES256Key-32Characters1234567890"))
 		assert.Equal(24, len(encrypted))
 	})
+
+	t.Run("Should return decrypted data", func(t *testing.T) {
+		sut := NewGCM()
+		nonce := make([]byte, 12)
+		secretKey := []byte("AES256Key-32Characters1234567890")
+		encrypted, _ := sut.Encrypt(nonce, []byte("any_data"), secretKey)
+		decrypted, _ := sut.Decrypt(nonce, encrypted, secretKey)
+		assert.Equal([]byte("any_data"), decrypted)
+	})
 }
