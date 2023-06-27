171f455e9fa9ffe815e7dc081e9d6cf94b5a9f45
D-Freitas <davi.israel01@gmail.com>
2023-06-27 09:39:18 -0300

    feature(aes256gcm): ensure this returns encrypted data

---
+++ gcm.go
@@ -0,0 +1,45 @@
+package aes256
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"errors"
+	"io"
+)
+
+type AES256GCM interface {
+	Encrypt(nonce []byte, data []byte, key []byte) ([]byte, error)
+}
+
+type gcm struct{}
+
+func NewGCM() AES256GCM {
+	return &gcm{}
+}
+
+func (e *gcm) Encrypt(nonce []byte, data []byte, key []byte) ([]byte, error) {
+	err := e.isNonceSizeValid(nonce)
+	if err != nil {
+		return nil, err
+	}
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		return nil, err
+	}
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		return nil, err
+	}
+	return aesgcm.Seal(nil, nonce, data, nil), nil
+}
+
+func (e *gcm) isNonceSizeValid(nonce []byte) error {
+	if len(nonce) != 12 {
+		return errors.New("nonce size must be equal to 12 bytes")
+	}
+	return nil
+}
